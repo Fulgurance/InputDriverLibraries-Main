cf46905e4afb10929ae1ef170a33ef218d339f34
fulgurance <zohran.londais@gmail.com>
2023-04-07 10:20:33 +0100

    Added input drivers installer

---
+++ Libinput/1.18.1/1.18.1.cr
@@ -0,0 +1,33 @@
+class Target < ISM::Software
+    
+    def prepare
+        super
+        @buildDirectory = true
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "-Ddebug-gui=false",
+                            "-Dtests=false",
+                            "--Ddocumentation=false",
+                            "-Dlibwacom=false",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end

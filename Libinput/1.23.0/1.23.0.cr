5e9f7dc3c3ec2553b560ee90a600de1c062cd31c
Fulgurance <zohran.londais@gmail.com>
2023-12-17 17:25:37 +0000

    Added missing argument

--- Libinput/1.23.0/1.23.0.cr
+++ Libinput/1.23.0/1.23.0.cr
@@ -4,7 +4,7 @@ class Target < ISM::Software
         @buildDirectory = true
         super
 
-        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"]],mainWorkDirectoryPath)
+        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"],"-Dlibwacom=false"],mainWorkDirectoryPath)
     end
 
     def configure
